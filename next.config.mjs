877fb6f34b8b96fa179280f8f6f7da3c485eb835
minhtrifit <minhtri.fit@gmail.com>
2024-02-03 00:16:34 +0700

    update supabase host config

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "piwwbijgpwvzynpsplfn.supabase.co",
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_HOST,
       },
     ],
   },
